0b2c1fd521cf0a3efe17d009181cfcc55228a92b
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-19 10:38:22 -0800

    created seed data, ratings.js post method and created + drop table

--- scripts/load-seed-data.js
+++ scripts/load-seed-data.js
@@ -39,6 +39,44 @@ const profiles = [
     hash: bcrypt.hashSync('abc', 8)
   }
 ];
+const ratings = [
+  {
+    user_name: 'ninja', 
+    score: 5
+  },
+  {
+    user_name: 'timthetatman', 
+    score: 3
+  },
+  {
+    user_name: 'shroud', 
+    score: 4
+  },
+  {
+    user_name: 'tyler1', 
+    score: 2
+  },
+  {
+    user_name: 'lirik', 
+    score: 4
+  },
+  {
+    user_name: 'yogscast', 
+    score: 3
+  },
+  {
+    user_name: 'dakotaz', 
+    score: 1
+  },
+  {
+    user_name: 'tfue', 
+    score: 5
+  },
+  {
+    user_name: 'dakotaz', 
+    score: 1
+  }
+];
 
 Promise.all(
   profiles.map(profile => {
@@ -63,6 +101,19 @@ Promise.all(
       })
     );
   })
+  .then(
+    () => {
+      return Promise.all(
+        ratings.map(rating => {
+          return client.query(`
+            INSERT INTO rating (user_name, score)
+            VALUES ($1, $2);
+          `,
+          [rating.user_name, rating.score])
+            .then(result => result.rows[0]);
+        })
+      );
+    })
   .then(
     () => console.log('seed data load complete'),
     err => console.log(err)
